c1779523059f5a4988e7924d429f36693f5672ed
Mostafa Radwan <mostafa.radwan@appdynamics.com>
2018-05-08 16:58:28 -0500

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => '',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDdxNk/HFsNoNs56X8qkjw9f2cphe8aOTstDrvi6OpcbjO4xyGq0Codofy76ZS5liK+LxK9I+0V+316+1BuhNU7bhIRnZQ4qdauNld5VldD+RKWt3OihJB+hqrvSidk8TLbSGRbutYv6iUJcmiVXqBjS6sjkcHicU2KGqSU9zD2GVz9qiKLWPypDkV0BqKeafiCHS4La81acXyVBQwN9KA0Vi59G2GFTv7u8Dd+DI+X6qaaFDCTbD3ywuzU+7LMJtFYUTC0Smkeu5TaAag3P/Kffa7e0UqH4GT+xMbBvV3BmUsNBRDq4NtwuzkL9yESIrXPv3Uqs5oCAgMszgS1M0Vf',
   }  
 }
