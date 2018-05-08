39d20f120e074365b5b51dd96222f4b647c10abf
Mostafa Radwan <mostafa.radwan@appdynamics.com>
2018-05-08 17:05:21 -0500

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -4,5 +4,15 @@ class profile::agent_nodes {
   dockeragent::node {'web.puppet.vm':}
   dockeragent::node {'db.puppet.vm':}
   
- 
+  host {'web.puppet.vm':
+    ensure  => present,
+    ip      => '10.0.2.15',
+  }
+  
+  host {'db.puppet.vm':
+    ensure  => present,
+    ip      => '172.18.0.3',
+  }
+  
+  
 }
