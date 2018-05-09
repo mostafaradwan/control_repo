f0500640991ce01f3a53a3073157b7f2a9057653
Mostafa Radwan <mostafa.radwan@appdynamics.com>
2018-05-09 10:28:42 -0500

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -4,7 +4,7 @@ class minecraft {
     ensure  => directory,
   }
 
-  file {'/opt/minecraft/minecraft_server.jar':
+  file {'/opt/minecraft/server.jar':
     ensure  => file,
     source  => 'https://launcher.mojang.com/mc/game/1.12.2/server/886945bfb2b978778c3a0288fd7fab09d315b25f/server.jar',
   }
