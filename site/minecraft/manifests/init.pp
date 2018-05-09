cca3c8fd28354ae75c841dc70817243bbce68af5
Mostafa Radwan <mostafa.radwan@appdynamics.com>
2018-05-09 09:14:24 -0500

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,26 +1,33 @@
 class minecraft {
 
-file {'/opt/minecraft':
-  ensure  => directory,
+  file {'/opt/minecraft':
+    ensure  => directory,
   }
 
-file {'/opt/minecraft/minecraft_server.jar':
-  ensure  => file,
-  source  => 'https://launcher.mojang.com/mc/game/1.12.2/server/886945bfb2b978778c3a0288fd7fab09d315b25f/server.jar',
+  file {'/opt/minecraft/minecraft_server.jar':
+    ensure  => file,
+    source  => 'https://launcher.mojang.com/mc/game/1.12.2/server/886945bfb2b978778c3a0288fd7fab09d315b25f/server.jar',
   }
 
-package{'java':
-  ensure  => present,
+  package{'java':
+   ensure  => present,
   }
 
-file {'/opt/minecraft/eula.txt':
-  ensure  => file,
-  contnet => 'eula=true',
+  file {'/opt/minecraft/eula.txt':
+    ensure  => file,
+    contnet => 'eula=true',
   }
 
-file {'/etc/systemd/system/minecraft.service':
-  ensure  => file,
-  source  => 'puppet:///modules/minecraft.service',
+  file {'/etc/systemd/system/minecraft.service':
+    ensure  => file,
+    source  => 'puppet:///modules/minecraft.service',
   }
+  
+  service{'minecraft':
+  
+    ensure  => running,
+    enable  => true,
+  }
+
   
 }
