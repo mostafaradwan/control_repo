c565902644e08d558b90f0ea41c3e181ab34f2cf
Mostafa Radwan <mostafa.radwan@appdynamics.com>
2018-05-17 18:38:29 -0500

    Update init.pp

--- site/jenkins/manifests/init.pp
+++ site/jenkins/manifests/init.pp
@@ -2,22 +2,28 @@ class jenkins {
 
   #Ensure the latest version of Java is installed since Jenkins requires JRE v1.8+
   package { 'java8':
-  
     name   => 'java-1.8.0-openjdk',
     ensure => installed,
   }
   
- package { "jenkins":
-  
+ #Install the package jenkins 
+ package { "jenkins":  
    #provider => rpm,
    #source   => "http://pkg.jenkins-ci.org/redhat/jenkins-2.122-1.1.noarch.rpm",
    ensure   => installed,
    #require  => Package['java8'],
 }
 
+#Alter /etc/sysconfig/jenkins and change JENKINS_PORT="8000" to JENKINS_PORT="8080"
+file_line { 'jenkins_port_line':
+  ensure =>  present,
+  path   => '/etc/sysconfig/jenkins',
+  line   => 'JENKINS_PORT="8080',
+  match  => '^\ JENKINS_PORT\=',
+}
+
 
  file {'/etc/systemd/system/jenkins.service':
- 
     ensure  => file,
     source  => 'puppet:///modules/jenkins/jenkins.service',
   }
@@ -26,7 +32,10 @@ class jenkins {
   
     ensure  => running,
     enable  => true,
-    require => [ Package['java8'] , File['/etc/systemd/system/jenkins.service'] ],
+    #require => [ Package['java8'] , File['/etc/systemd/system/jenkins.service'] ],
   }
+  
+  
+  Package['java8'] ~> Package['jenkins'] ~> File_line['jenkins_port_line'] ~> File['/etc/systemd/system/jenkins.service'] ~> Service['jenkins']
   
 }
