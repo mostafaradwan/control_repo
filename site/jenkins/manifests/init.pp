29c1f266299e7d695a3e40d98d717f182d227c02
Mostafa Radwan <mostafa.radwan@appdynamics.com>
2018-05-17 17:45:49 -0500

    Update init.pp

--- site/jenkins/manifests/init.pp
+++ site/jenkins/manifests/init.pp
@@ -1,18 +1,32 @@
 class jenkins {
 
   #Ensure the latest version of Java is installed since Jenkins requires JRE v1.8+
-  package { 'java-1.8.0-openjdk':
+  package { 'java8':
   
-    #name   => 'java-1.8.0-openjdk',
+    name   => 'java-1.8.0-openjdk',
     ensure => installed,
   }
   
  package { "jenkins":
   
- provider => rpm,
- source   => "http://pkg.jenkins-ci.org/redhat/jenkins-2.122-1.1.noarch.rpm",
- ensure   => installed,
- require  => Package['java-1.8.0-openjdk'],
+   provider => rpm,
+   source   => "http://pkg.jenkins-ci.org/redhat/jenkins-2.122-1.1.noarch.rpm",
+   ensure   => installed,
+   require  => Package['java-1.8.0-openjdk'],
 }
+
+
+ file {'/etc/systemd/system/jenkins.service':
  
+    ensure  => file,
+    source  => 'puppet:///modules/jenkins/jenkins.service',
+  }
+  
+ service{'minecraft':
+  
+    ensure  => running,
+    enable  => true,
+    require => [ Package['java8'] , File['/etc/systemd/system/jenkins.service'] ],
+  }
+  
 }
