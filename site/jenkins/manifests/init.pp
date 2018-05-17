431f84908413af1b8b043677fccc7d086cbe8906
Mostafa Radwan <mostafa.radwan@appdynamics.com>
2018-05-17 10:16:33 -0500

    Update init.pp

--- site/jenkins/manifests/init.pp
+++ site/jenkins/manifests/init.pp
@@ -7,4 +7,18 @@ class jenkins {
     ensure => latest,
   }
   
+  #Add the Jenkins repo to yum in case it's not there
+  yumrepo { "jenkins-yum-repo":
+  
+    baseurl => "http://pkg.jenkins-ci.org/redhat/jenkins.repo",
+     enabled => 1,
+}
+  
+  #Install Jenkins
+  package { 'jenkins':
+  
+     ensure  => present,
+     require => Yumrepo["jenkins-yum-repo"],
+}
+  
 }
