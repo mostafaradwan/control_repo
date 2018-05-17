fa13a4c5ec759f4808af1b2df57f78396353a7c9
Mostafa Radwan <mostafa.radwan@appdynamics.com>
2018-05-17 09:32:18 -0500

    Update init.pp

--- site/jenkins/manifests/init.pp
+++ site/jenkins/manifests/init.pp
@@ -1,13 +1,8 @@
 class jenkins {
 
-
-  #package { 'java-1.8.0-openjdk':
-  #ensure => latest,
-  #}
-  
   #Ensure the latest version of Java is installed since Jenkins requires JRE v1.8+
-  package { 'java':
-    ensure =>  'latest',
+  package { 'java-1.8.0-openjdk':
+  ensure => latest,
   }
   
 }
