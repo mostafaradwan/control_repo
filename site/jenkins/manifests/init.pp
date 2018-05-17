38fe6deb43c8726c9a0ce3ba115dc1b23793f8b7
Mostafa Radwan <mostafa.radwan@appdynamics.com>
2018-05-17 12:07:29 -0500

    Update init.pp

--- site/jenkins/manifests/init.pp
+++ site/jenkins/manifests/init.pp
@@ -1,9 +1,9 @@
 class jenkins {
 
   #Ensure the latest version of Java is installed since Jenkins requires JRE v1.8+
-  package { 'java8':
+  package { 'java-1.8.0-openjdk':
   
-    name   => 'java-1.8.0-openjdk',
+    #name   => 'java-1.8.0-openjdk',
     ensure => latest,
   }
   
@@ -12,7 +12,7 @@ class jenkins {
     provider => rpm,
     source   => "http://pkg.jenkins-ci.org/redhat/jenkins-2.122-1.1.noarch.rpm",
     ensure   => installed,
-    require  => Package['java8'],
+    require  => Package['java-1.8.0-openjdk'],
 }
  
 }
