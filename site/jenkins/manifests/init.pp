4a7769e7d5b32a85830c144a6e480f98b3f07add
Mostafa Radwan <mostafa.radwan@appdynamics.com>
2018-05-18 16:28:51 -0500

    Update init.pp

--- site/jenkins/manifests/init.pp
+++ site/jenkins/manifests/init.pp
@@ -1,43 +1,51 @@
 class jenkins {
 
-  #Ensure the latest version of Java is installed since Jenkins requires JRE v1.8+
-  package { 'java8':
-    name   => 'java-1.8.0-openjdk',
-    ensure => installed,
-  }
+
+ #Add the jenkins to the yum repo along the GPG key
+     yumrepo { 'jenkins_repo':
+     
+      baseurl  => "http://pkg.jenkins-ci.org/redhat",
+      descr    => "Jenkins",
+      enabled  => 1,
+      gpgcheck => 1,
+      gpgkey   => "http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key",
+      }
+
+
+ #Ensure the latest version of Java is installed since Jenkins requires JRE v1.8+
+    package { 'java8':
+      name   => 'java-1.8.0-openjdk',
+      ensure => installed,
+    }
   
- #Install the package jenkins 
- package { "jenkins":  
-   #provider => rpm,
-   #source   => "http://pkg.jenkins-ci.org/redhat/jenkins-2.122-1.1.noarch.rpm",
-   ensure   => installed,
-   #require  => Package['java8'],
-}
+  #Install the package jenkins
+    package { 'jenkins':
+       ensure   => latest,
+    }
 
-#Alter /etc/sysconfig/jenkins and change the default JENKINS_PORT="8080" to JENKINS_PORT="8000" 
+#Alter /etc/sysconfig/jenkins and change the default JENKINS_PORT="8080" to JENKINS_PORT="8000"
 #Using the defined type file_line in the stdlib
-file_line { 'jenkins_port_line':
-  ensure             =>  present,
-  path               => '/etc/sysconfig/jenkins',
-  line               => 'JENKINS_PORT=8000',
-  match              => 'JENKINS_PORT=',
-  append_on_no_match => false,
-}
+    file_line { 'jenkins_port_line':
+      ensure             =>  present,
+      path               => '/etc/sysconfig/jenkins',
+      line               => 'JENKINS_PORT="8000"',
+      match              => '^JENKINS_PORT=',
+      append_on_no_match => false,
+    }
 
-
+#Create the service file for the jenkins service
  file {'/etc/systemd/system/jenkins.service':
     ensure  => file,
     source  => 'puppet:///modules/jenkins/jenkins.service',
   }
   
+ #Ensure the jenkins service is running 
  service{'jenkins':
-  
     ensure  => running,
     enable  => true,
     #require => [ Package['java8'] , File['/etc/systemd/system/jenkins.service'] ],
   }
   
   
-  Package['java8'] ~> Package['jenkins'] ~> File_line['jenkins_port_line'] ~> File['/etc/systemd/system/jenkins.service'] ~> Service['jenkins']
-  
+ Yumrepo['jenkins_repo'] ~> Package['java8'] ~> Package['jenkins'] ~> File_line['jenkins_port_line'] ~> File['/etc/systemd/system/jenkins.service'] ~> Service['jenkins']  
 }
